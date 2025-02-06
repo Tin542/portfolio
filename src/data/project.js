7bbe3bbcaaf50672332b2557bdb1353e8e1973c3
bin <tinnt0504@gmail.com>
2025-02-06 12:30:43 +0700

    update: fix bug + update CV

--- src/data/project.js
+++ src/data/project.js
@@ -1,7 +1,17 @@
 import book from "../Assets/Projects/book.png";
 import portfolio from "../Assets/Projects/portfolio.png";
+import tinbot from "../Assets/Projects/tinbot.png";
 
 export const ProjectsData = [
+  {
+    imgPath: tinbot,
+    title: "TinBot",
+    description:
+      `A chatbot application that provides automated responses to user queries, offering support and interaction across multiple platforms`,
+    ghLink: "https://github.com/Tin542/chatbot",
+    demoLink: "https://tinbot.vercel.app/",
+    isBlog: false,
+  },
   {
     imgPath: book,
     title: "BookStore",
